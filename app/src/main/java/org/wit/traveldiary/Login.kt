15101a1de2788fedf8b612cc5265cb41020962ab
Maeliosa Leamy <59532461+red28mil@users.noreply.github.com>
2022-01-01 00:26:09 +0000

    Login

--- app/src/main/java/org/wit/traveldiary/Login.kt
+++
@@ -1,4 +0,0 @@
-package org.wit.traveldiary
-
-class Login {
-}
--- app/src/main/java/org/wit/traveldiary/MainActivity.kt
+++ app/src/main/java/org/wit/traveldiary/MainActivity.kt
@@ -1,11 +1,31 @@
 package org.wit.traveldiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
+
+        val button = findViewById<Button>(R.id.button)
+// set on-click listener
+        button.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+           
+
+
+        }
     }
 }
