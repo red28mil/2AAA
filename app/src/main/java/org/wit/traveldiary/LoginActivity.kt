11c6b817a628cfad875011de9c3e51ef35292902
Maeliosa Leamy <59532461+red28mil@users.noreply.github.com>
2022-01-01 18:29:45 +0000

    google

--- app/src/main/java/org/wit/traveldiary/LoginActivity.kt
+++ app/src/main/java/org/wit/traveldiary/LoginActivity.kt
@@ -1,13 +1,27 @@
 
 package org.wit.traveldiary
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+
 import androidx.appcompat.app.AppCompatActivity
+
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //get layout of page from xml
         setContentView(R.layout.activity_login)
+        val button2 = findViewById<Button>(R.id.button2)
+        // set on-click listener
+        button2.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/org/wit/traveldiary/MainActivity.kt
+++ app/src/main/java/org/wit/traveldiary/MainActivity.kt
@@ -7,25 +7,78 @@ import android.widget.AdapterView
 import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
+const val  REQUEST_CODE_SIGN_IN  = 0
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
-
         val button = findViewById<Button>(R.id.button)
-// set on-click listener
+        // set on-click listener
         button.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-           
+            Intent(this, LoginActivity::class.java).also{
+                startActivity(it)
+            }
+        }
 
+       auth = FirebaseAuth.getInstance()
+        //for google
+        val googleSignInButton = findViewById<Button>(R.id.googleSignInButton)
+        // set on-click listener
+        googleSignInButton.setOnClickListener {
+            val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build()
+            //val googleSignInClient = GoogleSignIn.getClient(this, gso)
+            val signInClient = GoogleSignIn.getClient(this, options)
+            signInClient.signInIntent.also{
+                startActivityForResult(it, REQUEST_CODE_SIGN_IN)
+
+            }
+
+        }
+
+    }
+    private fun googleAuthForFirebase(account: GoogleSignInAccount){
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch{
+            try{
+                auth.signInWithCredential(credentials).await()
+                Toast.makeText(this@MainActivity, "success", Toast.LENGTH_LONG).show()
+            }catch(e: Exception){
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_SIGN_IN){
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let{
+                googleAuthForFirebase(it)
+            }
 
         }
     }
+
 }
+
